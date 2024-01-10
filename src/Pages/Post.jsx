9f50f4593c87841af49a398b47091e3075f960c2
Alisher Satimov <alishersatimov07@gmail.com>
2024-01-11 00:17:03 +0500

    post finished

--- src/Pages/Post.jsx
+++ src/Pages/Post.jsx
@@ -8,7 +8,19 @@ const Post = () => {
 
   if (!postId) return <Navigate to="/posts" />;
 
-  return isLoading ? <Spinner /> : post && <div>Post</div>;
+  return isLoading ? (
+    <Spinner />
+  ) : (
+    post && (
+      <div className="container d-flex justify-content-center align-items-center flex-column gap-4 mt-4">
+        <img src={post.avatar} width={200} alt="" />
+        <h1>{post.name}</h1>
+        <h2 className="text-center">{post.text}</h2>
+        <p>Posted: {post.date.slice(0, 10)}</p>
+        {console.log(post)}
+      </div>
+    )
+  );
 };
 
 export default Post;
